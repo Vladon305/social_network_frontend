9e25a2d73c2a09b9314abd79c8db75082ba294b5
Vladon305 <shuhow003@mail.ru>
2022-07-25 22:24:50 +0300

    initial commit

---
+++ src/components/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type PropsType = {}
+
+const Home: React.FC<PropsType> = ({ }) => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
---
+++ src/components/Login.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type PropsType = {}
+
+const Login: React.FC<PropsType> = ({ }) => {
+  return (
+    <div>login</div>
+  )
+}
+
+export default Login
